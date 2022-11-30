76a9a1842aa289f5bcdf1aee17890bd9d32d2ff9
Resi Wicaksono <resiwicaksono98@gmail.com>
2022-12-01 02:44:36 +0700

    Fix Bug Auth and change middleware

--- app.js
+++ app.js
@@ -1,63 +1,87 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors')
-const {decodeToken} = require('./middleware/index')
-const productRoutes = require('./app/product/router')
-const categoryRoutes = require('./app/category/router')
-const tagRoutes = require('./app/tags/router')
-const authRoutes = require('./app/auth/router')
-const deliveryAddressRoutes = require('./app/deliveryAddress/router')
-const cartRoutes = require('./app/cart/router')
-const orderRoutes = require('./app/order/router')
-const invoiceRoutes = require('./app/invoice/router')
-
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const productRoutes = require("./app/product/router");
+const categoryRoutes = require("./app/category/router");
+const tagRoutes = require("./app/tags/router");
+const authRoutes = require("./app/auth/router");
+const deliveryAddressRoutes = require("./app/deliveryAddress/router");
+const cartRoutes = require("./app/cart/router");
+const orderRoutes = require("./app/order/router");
+const invoiceRoutes = require("./app/invoice/router");
+const { secretKey } = require("./app/config");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(cors())
-app.use(logger('dev'));
+const store = new MongoDBStore({
+  uri: "mongodb://localhost:27017/foodstore",
+  collection: "session",
+});
+// Set Session
+app.use(
+  session({
+    name: "kepo",
+    secret: secretKey,
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+    cookie: {
+      secure: "auto",
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 1, // 1 Day
+    },
+  })
+);
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173", "http://localhost:4173"],
+  })
+);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(decodeToken())
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api', productRoutes)
-app.use('/api', categoryRoutes)
-app.use('/api', tagRoutes)
-app.use('/auth', authRoutes)
-app.use('/api', deliveryAddressRoutes)
-app.use('/api', cartRoutes)
-app.use('/api', orderRoutes)
-app.use('/api', invoiceRoutes)
+app.use("/api", productRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", tagRoutes);
+app.use("/auth", authRoutes);
+app.use("/api", deliveryAddressRoutes);
+app.use("/api", cartRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", invoiceRoutes);
 // Home
-app.use('/', function(req,res) {
-   res.render('index', {
-     title: 'Food Server'
-   })
-})
+app.use("/", function (req, res) {
+  res.render("index", {
+    title: "Food Server",
+  });
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- app/auth/router.js
+++ app/auth/router.js
@@ -1,13 +1,15 @@
-const router = require('express').Router()
-const authController = require('./controller')
-const passport = require('passport')
-const localStrategy = require('passport-local').Strategy
+const router = require("express").Router();
+const authController = require("./controller");
+const passport = require("passport");
+const { verifyUser } = require("../../middleware");
+const localStrategy = require("passport-local").Strategy;
 
-passport.use(new localStrategy({usernameField: 'email'}, authController.localStrategy))
-router.post('/register', authController.register)
-router.post('/login', authController.login)
-router.post('/logout', authController.logout)
-router.get('/me',authController.me)
+passport.use(
+  new localStrategy({ usernameField: "email" }, authController.localStrategy)
+);
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.post("/logout", authController.logout);
+router.get("/me", verifyUser, authController.me);
 
-
-module.exports = router
+module.exports = router;
