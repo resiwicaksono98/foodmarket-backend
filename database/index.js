007f893985490b5ca80becccbf532a1a8489c6da
Resi Wicaksono <resiwicaksono98@gmail.com>
2023-01-16 14:36:29 +0700

    change db to atlas

--- database/index.js
+++ database/index.js
@@ -1,9 +1,9 @@
+/** @format */
+
 const mongoose = require("mongoose");
 const { dbHost, dbPass, dbName, dbPort, dbUser } = require("../app/config");
 
-mongoose.connect(
-  `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`
-);
+mongoose.connect(`mongodb+srv://resiwicaksono:thonkwaq123@cluster0.a3dlh.mongodb.net/wakburgerbar?retryWrites=true&w=majority`);
 const db = mongoose.connection;
 
 module.exports = db;
