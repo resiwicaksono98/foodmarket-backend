a4bff40e1eb72f5057195afc1489e1867d151284
Resi Wicaksono <resiwicaksono98@gmail.com>
2023-01-17 14:43:00 +0700

    set packagejson

--- middleware/index.js
+++ middleware/index.js
@@ -1,23 +1,22 @@
-const { getToken, policyFor } = require("../utils");
+/** @format */
+
 const jwt = require("jsonwebtoken");
 const config = require("../app/config");
 const User = require("../app/user/model");
 
 const verifyUser = async (req, res, next) => {
-  if (!req.session.token)
-    return res.status(401).json({ message: "Please login your account" });
-  let token = req.session.token;
-  if (!jwt.JsonWebTokenError)
-    return res.status(401).json({ msg: "Please login your account" });
-  req.user = jwt.verify(token, config.secretKey);
-  let user = await User.findOne({ token: { $in: [token] } });
-  if (!user) {
-    res.status(500).json({
-      error: 1,
-      message: "Token Expired",
-    });
-  }
-  next();
+   if (!req.session.token) return res.status(401).json({ message: "Please login your account" });
+   let token = req.session.token;
+   if (!jwt.JsonWebTokenError) return res.status(401).json({ msg: "Please login your account" });
+   req.user = jwt.verify(token, config.secretKey);
+   let user = await User.findOne({ token: { $in: [token] } });
+   if (!user) {
+      res.status(500).json({
+         error: 1,
+         message: "Token Expired",
+      });
+   }
+   next();
 };
 // function decodeToken() {
 //   return async function (req, res, next) {
@@ -47,17 +46,4 @@ const verifyUser = async (req, res, next) => {
 
 // Policy Check
 
-function police_check(action, subject) {
-  return function (req, res, next) {
-    let policy = policyFor(req.user);
-    if (!policy.can(action, subject)) {
-      return res.json({
-        error: 1,
-        message: `You Are Not Allowed To ${action} ${subject}`,
-      });
-    }
-    next();
-  };
-}
-
-module.exports = { verifyUser, police_check };
+module.exports = { verifyUser };
